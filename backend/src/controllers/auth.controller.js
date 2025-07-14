aa46c79a958e427afdc91787064b178935328f4f
Nishant chahar <nishantchahar20@gmail.com>
2025-07-14 15:20:10 +0530

    signup route created

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,7 +1,64 @@
 import e from "express";
+import User from "../models/User.js";
+import jwt from 'jsonwebtoken';
 
-export async function signup(req ,  res) {
-  res.send("Signup endpoint");
+export async function signup(req ,  res) { 
+    const {fullName , email ,  password} = req.body;
+
+  try{
+      if (!fullName || !email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
+    if(password.length < 6){
+       return  res.status(400).json({ message: "Password must be at least 6 characters long" });
+    }
+
+    const emailregex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailregex.test(email)) {
+        return res.status(400).json({ message: "Invalid email format" });
+    }
+
+    const existingEmail = await User.findOne({ email});
+    if (existingEmail) {
+        return res.status(400).json({ message: "Email already exists" });
+    }
+    const idx =  Math.floor(Math.random() * 100)+1;
+
+    const randomProfilePicture = `https://avatar.iran.liara.run/public/${idx}.png`;
+    const newUser =  await User.create({
+        fullName,
+        email,
+        password,
+        profilePicture: randomProfilePicture,
+       
+    });
+
+    const token = jwt.sign({userid : newUser._id}, process.env.JWT_SECRET, {
+        expiresIn: '7d'});
+
+     res.cookie('jwt', token, {
+        httpOnly: true,  // prevent xss attacks
+        secure: process.env.NODE_ENV === 'production', 
+        maxAge: 7 * 24 * 60 * 60 * 1000 ,// 7 days 
+        sameSite: 'strict' // prevent CSRF attacks
+    });
+
+
+    res.status(201).json({
+        message: "User created successfully",
+        user: newUser
+    });
+
+
+  }
+  catch(error) {
+    console.error("Signup error:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+
+   
+
 }
 
 export async  function login(req, res) {
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,12 +4,17 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 import authRoutes from './routes/auth.route.js';
+import { connectDB } from './lib/db.js';
 
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
 
 
+
+
 app.listen(PORT , () => {
     console.log(`Server is running on port ${PORT}`);
+    connectDB()
 })
