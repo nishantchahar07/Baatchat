be208a9834e8b747cbd41ca0198608f0df6066cf
Nishant chahar <nishantchahar20@gmail.com>
2025-07-15 18:29:05 +0530

    app page  setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,35 @@
+import { useEffect } from "react";
+import { Toaster } from "react-hot-toast"
+import { Route, Routes } from "react-router"
 
 function App() {
+  const [data , setData] = useState([]);
+  const [isLoading , setIsLoading ] = useState(false);
+  const [error , setError] = useState(null);
+
+  useEffect(() => {
+    const getData = async () => {}});
+
+
+
    return (
    <>
-   <h1 className='bg-amber-500 text-white '>chaudhar jaat ki</h1>
+   <div claddName='h-screen' data-theme="lemonade">
+//TODO : read doc about react query tanstack query 
+    <Routes>
+      <Route path ="/" element={<HomePage/>} />
+      <Route path ="/signup" element={<SignUpPage/>} />
+      <Route path ="/login" element={<LoginPage/>} />
+      <Route path ="/notification" element={<NotificationPage/>} />
+      <Route path ="/call" element={<CallPage/>} />
+      <Route path ="/chat" element={<ChatPage/>} />
+      <Route path ="/onboarding" element={<OnboardingPage/>} />
+    </Routes>
+
+    <Toaster/>
+    
+   </div>
+   
    </>
   )
 }
