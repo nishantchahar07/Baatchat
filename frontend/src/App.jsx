e48f4148c8b7c35a0845fab80a802da4614bbd3d
Nishant chahar <nishantchahar20@gmail.com>
2025-07-15 16:18:58 +0530

    pages added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+   return (
    <>
    <h1 className='bg-amber-500 text-white '>chaudhar jaat ki</h1>
    </>
