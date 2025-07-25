145251ca6ad65a684854882a66aab3b2910e0a04
Nishant chahar <nishantchahar20@gmail.com>
2025-07-25 08:35:44 +0530

    slight changes in package

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,21 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "stream-chat-react/dist/css/v2/index.css";
+import "./index.css";
+import App from "./App.jsx";
 
+import { BrowserRouter } from "react-router";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')).render(
-
-  <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </BrowserRouter>
-
-)
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
